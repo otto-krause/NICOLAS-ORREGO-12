c6119e12ad486fac3cf0c4d980ef9f9801f9626c
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-09-18 14:49:31 -0300

    Update ej2funciones.c

--- ej2funciones.c
+++ ej2funciones.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
-int perimetro(int a, int b){
-	int p;
-	p=2*a+2*b;
+int perimetro(int a,int b){
+  int p;
+  p=2*a+2*b;
+  return p;
 }
-int area(int a, int b){
-	int ar; 
-	ar=a*b;
+int area(int a,int b){
+  int ar;
+  ar=a*b;
+  return ar;
 }
 int main(){
-	int a,b;
-	scanf("%d",&a);
-	scanf("%d",&b);
-	
-    int ar2= area(a,b);
-    int p2= perimetro(a,b);
-	
-printf("el area es: %d",ar2);
-printf("\nel perimetro es: %d",p2);
+ int a,b;
+ scanf("%d",&a);
+ scanf("%d",&b);
+
+ int res1=perimetro(a,b);
+ int res2=area(a,b);
+
+ printf("el perimetro es: %d\n",res1);
+ printf("el area es: %d\n",res2);
 }
