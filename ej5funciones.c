bccb3b4cf6c24e48f69690101363f9abee9b5739
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-10-10 13:37:30 -0300

    Update ej5funciones.c

--- ej5funciones.c
+++ ej5funciones.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int suma(int a,int i,int t){
-	for(i=0;i<5;i++){
+int suma(int a,int t){
+	for(int i=0;i<5;i++){
 	scanf("%d",&a);
 	t+=a;
 	}
 	return t;
 }
 int main(){
-	int a=0,i=0,t=0;
+	int a=0,t=0;
 	printf("ingrese 5 resistencias: ");
-	int res=suma(a,i,t);
+	int res=suma(a,t);
 	printf("el resultado es: %d\n",res);
 }
