51478e766e670374ae5a9667944c90aaab961ff3
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-10-10 18:05:13 -0300

    Update ej7funciones.c

--- ej7funciones.c
+++ ej7funciones.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
-int num(int n1, int n2){
-	if(n1>n2){
-		printf("el primer numero es mayor al segundo");
-	}
-	else if(n1<n2){
-		printf("el segundo numero es mayor al primero");
-	}
-	else{
-		printf("los numeros son iguales");
-	}
+int regreso(int a, int b){
+if(a>b){
+  return 1;
+ }
+ else if(a<b){
+   return 2;
+ }
+ else{
+   return 3;
+ }
 }
-
 int main(){
-	int a,b;
-	
-	printf("ingrese dos numeros: ");
-	scanf("%d",&a);
-	scanf("%d",&b);
-	
-	int res= num(a,b);
+  int n1,n2;
+  printf("ingrese dos numeros: ");
+  scanf("%d %d",&n1,&n2);
+
+  int res=regreso(n1,n2);
+
+  if(res==1){
+    printf("el primer numero es mayor al segundo\n");
+  }
+  else if(res==2){
+    printf("el segundo numero es mayor al primero\n");
+  }
+  else if(res==3){
+    printf("los numeros son iguales\n");
+  }
 }
