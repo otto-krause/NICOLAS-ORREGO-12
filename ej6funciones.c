6942cfd075e3881bc522575bd61f781491e03d43
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-10-10 13:40:44 -0300

    Update ej6funciones.c

--- ej6funciones.c
+++ ej6funciones.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int octaba(int a){
-	int total;
+float octaba(float a){
+	float total;
 	int b=8;
 	total=a/b;
 	return total;
 }
 int main(){
-	int n1;
+	float n1;
 	printf("ingrese el numero: ");
 	scanf("%d",&n1);
-	int res= octaba(n1);
-	printf("el resultado es: %d",res);
+	float res= octaba(n1);
+	printf("el resultado es: %f",res);
 }
